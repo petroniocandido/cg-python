5cc6706a264b19e861cb2045176f86eb4a436311
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2022-09-13 09:07:31 -0300

    Create animacoes.py

---
+++ animacoes.py
@@ -0,0 +1,24 @@
+import numpy as np
+import PIL
+from PIL import Image
+
+def animar(num_frames, duracao, funcao_animacao, largura, altura, arquivo_gif):
+  #LOOP PRINCIPAL
+  
+  # LISTA DOS FRAMES DA ANIMAÇÃO
+  frames = []
+
+  # REPETE DE 0 ATÉ O NÚMERO DE FRAMES
+  for i in range(num_frames):
+
+    # CRIA A NOVA IMAGEM VAZIA
+    tela = Image.new(mode="RGBA", size=(altura,largura), color="white")
+
+    # PASSA A IMAGEM EM BRANCO E O NÚMERO DO FRAME PARA A FUNÇÃO DE ANIMAÇÃO
+    new_frame = funcao_animacao(tela, i) 
+
+    # ADICIONA O NOVO FRAME GERADO À LISTA DE FRAMES
+    frames.append(new_frame)
+
+  # SALVA A ANIMAÇÃO EM UM ARQUIVO .GIF, COM 40 MILISEGUNDOS ENTRE CADA FRAME
+  frames[0].save(arquivo_gif, format='GIF', append_images=frames[1:], save_all=True, duration=duracao, loop=0)

b14824e8899f62d93941c783369bba8b2073ebee
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2022-09-13 10:44:07 -0300

    Update comum.py

--- comum.py
+++ comum.py
@@ -3,5 +3,5 @@ import PIL
 from PIL import Image
 
 def putpixel(imagem, x, y, cor=(50,50,50,255)):
-  if x >= 0 and x <= imagem.width and y >= 0 and y <= imagem.height:
+  if x >= 0 and x < imagem.width and y >= 0 and y < imagem.height:
     imagem.putpixel((int(x),int(y)), cor)

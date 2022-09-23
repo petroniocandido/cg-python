bf114b338efe060b755f0ba2d585f44bc819db3b
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2022-09-23 15:42:43 -0300

    Update transformacoes.py

--- transformacoes.py
+++ transformacoes.py
@@ -60,3 +60,58 @@ class MatrizTransformacao2D(object):
 
   def str(self):
     return str(self.matriz)
+  
+  
+class MatrizTransformacao3D(object):
+
+  # Construtor
+  def __init__(self):
+    self.matriz = np.eye(4,4)
+
+  def limpar(self):
+    self.matriz = np.eye(4,4)
+
+  def transformar(self, tx, ty, tz, ex, ey, ez, rx, ry, rz):
+    self.translacao(tx, ty, tz)
+    self.escala(ex, ey, ez)
+    self.rotacao(rx, ry, rz)
+    return self
+
+  def translacao(self, tx, ty, tz):
+    self.matriz[3,0] += tx
+    self.matriz[3,1] += ty
+    self.matriz[3,2] += tz
+    return self
+
+  def escala(self, ex, ey, ez):
+    self.matriz[0,0] = ex if self.matriz[0,0] == 1 else ex + self.matriz[0,0]
+    self.matriz[1,1] = ey if self.matriz[1,1] == 1 else ey + self.matriz[1,1]
+    self.matriz[2,2] = ex if self.matriz[2,2] == 1 else ez + self.matriz[2,2]
+    return self
+
+  def rotacao(self, rx, ry, rz):
+    self.matriz[1,1] = np.cos(rx) if self.matriz[1,1] == 1 else np.cos(rx) + self.matriz[1,1]
+    self.matriz[1,2] -= np.sin(rx)
+    self.matriz[2,1] += np.sin(rx)
+    self.matriz[2,2] = np.cos(rx) if self.matriz[2,2] == 1 else np.cos(rx) + self.matriz[2,2]
+
+    self.matriz[0,0] = np.cos(ry) if self.matriz[0,0] == 1 else np.cos(ry) + self.matriz[0,0]
+    self.matriz[0,2] += np.sin(ry)
+    self.matriz[2,0] -= np.sin(ry)
+    self.matriz[2,2] = np.cos(ry) if self.matriz[2,2] == 1 else np.cos(ry) + self.matriz[2,2]
+
+    self.matriz[0,0] = np.cos(rz) if self.matriz[0,0] == 1 else np.cos(rz) + self.matriz[0,0]
+    self.matriz[0,1] += -np.sin(rz)
+    self.matriz[1,0] += np.sin(rz)
+    self.matriz[1,1] = np.cos(rz) if self.matriz[1,1] == 1 else np.cos(rz) + self.matriz[1,1]
+
+    return self
+
+
+  def aplicar(self, ponto):
+    p = np.array([ponto.x, ponto.y, ponto.z, 1])
+    pt = p.dot(self.matriz)
+    return Ponto5D(pt[0], pt[1], pt[2])
+
+  def str(self):
+    return str(self.matriz)
